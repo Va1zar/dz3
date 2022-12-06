ea6bfd4838f2c44b7862996f26f1b9d1db044186
Evgeniy <s.h.a.r.k.112@mail.ru>
2022-12-06 16:11:54 +0700

    solve of task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел 
+//от 1 до N.
+
+Console.WriteLine("Введите число = ");
+int A = int.Parse(Console.ReadLine()!);
+int B = 1;
+while (B <= A)
+{
+    if (B == A)
+    {
+        Console.Write(B * B * B + ". ");
+    }
+    else
+    {
+        Console.Write(B * B * B + ", ");
+    }
+    B++;
+}
